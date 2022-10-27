1a7012c2980c2db8d8b1867f2576b4f5ddb2867b
stefaniazocco <stefaniazocco93@gmail.com>
2022-10-27 18:59:53 +0200

    feat: add Cookies component

---
+++ src/components/cookies/Cookies.js
@@ -0,0 +1,31 @@
+import cookies from "../../assets/images/homepage/logo-cookies.png";
+import xIcon from "../../assets/images/homepage/x-icon.png";
+
+export function Cookies({hide}){
+
+
+    return(
+        <section id="cookies-section">
+            <img className="logo-cookies" src={cookies} />
+            <div className="cookies-text">Sony Interactive Entertainment Europe Limited utilizza i cookie per personalizzare la
+                tua esperienza e gli annunci pubblicitari su questo sito Web e su siti Web di terze parti, analizzare i
+                dati, migliorare le prestazioni del sito e consentire la condivisione sui social media. Per ulteriori
+                informazioni o per modificare le impostazioni, <a href="#">consulta la nostra Informativa sui cookie</a> .
+            </div>
+            <div className="cookies-buttons">
+                <div className="cookies-Button" onClick={hide}>
+                    <a href="#">
+                        <div>Rifiuta</div>
+                    </a>
+                </div>
+                <div className="cookies-Button" onClick={hide}>
+                    <a href="#">
+                        <div>Accetta</div>
+                    </a>
+                </div>
+            </div>
+
+            <button className="close-tab" onClick={hide}><img src={xIcon} /></button>
+        </section>
+    )
+}
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
+import { Cookies } from "../../components/cookies/Cookies";
 import { Button } from "../../components/elements/buttons/Button";
 import { Counter } from "../../components/elements/counter/StoreCounter";
 import { FooterMain } from "../../components/footer/FooterMain";
 
 
 export function Homepage(){
+    const [close, setClose] = useState(true)
+
+    function hideSection() {
+        setClose(false)
+    }
+
     return (
         <>
             <Button className="button-red" link="#" string="Maggiori informazioni"/>
             <Counter />
             <FooterMain />
+            {close === true && <Cookies hide={hideSection} />}
         </>
     )
 }
