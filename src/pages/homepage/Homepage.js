7cf3140457302ac84f67ce5dd5bddd8656e62e81
stefaniazocco <stefaniazocco93@gmail.com>
2022-10-27 19:29:24 +0200

    feat: add Socials component

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Cookies } from "../../components/cookies/Cookies";
 import { Button } from "../../components/elements/buttons/Button";
 import { Counter } from "../../components/elements/counter/StoreCounter";
+import { Socials } from "../../components/elements/sections/socials/Socials";
 import { FooterMain } from "../../components/footer/FooterMain";
 
 
@@ -16,7 +17,9 @@ export function Homepage(){
         <>
             <Button className="button-red" link="#" string="Maggiori informazioni"/>
             <Counter />
+            <Socials />
             <FooterMain />
+            
             {close === true && <Cookies hide={hideSection} />}
         </>
     )
