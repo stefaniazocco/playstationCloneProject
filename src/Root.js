9c2dc06c1a0676125d22eba72a4e8aa7506e3f4f
stefaniazocco <stefaniazocco93@gmail.com>
2022-11-20 20:14:29 +0100

    feat: add scroll on top component

--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ScrollTop } from "./components/elements/Scroll";
 
 export function Root(){
     return (
         <BrowserRouter>
+        <ScrollTop />
             <App />
         </BrowserRouter>
     )
