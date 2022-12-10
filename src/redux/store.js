1a8261753f66ce19c3c84391ae71541c49f12ad8
AngeloC94 <tripway7777@gmail.com>
2022-12-10 12:13:42 +0100

    pull stefi

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,40 @@
 import { combineReducers, createStore } from "redux";
 import { productReducer, selectReducer } from "./CartReducer.js";
 import { counterReducer } from "./CounterReducer.js";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./userRedux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
+const rootReducer = combineReducers({ user: userReducer, product: productReducer,
+  size: selectReducer,
+  counter: counterReducer });
 
-const rootReducer = combineReducers({
-    product: productReducer,
-    size: selectReducer,
-    counter: counterReducer
-})
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(rootReducer)
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
+export let persistor = persistStore(store);
