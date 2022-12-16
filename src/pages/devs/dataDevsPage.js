315a4eaf7bea08d2da449b0f0cd9e665a140aaed
RiccardoPriolo <riccardo.priolo01@gmail.com>
2022-12-16 20:13:17 +0100

    feat/ devPage

--- src/pages/devs/dataDevsPage.js
+++ src/pages/devs/dataDevsPage.js
@@ -4,13 +4,16 @@ import marco from "../../assets/images/devsPageAssets/qr-code/qr-code-marco.png"
 import riccardo from "../../assets/images/devsPageAssets/qr-code/qr-code-riccardo.png";
 import stefania from "../../assets/images/devsPageAssets/qr-code/qr-code-stefania.png";
 
+import angeloFoto from "../../assets/images/devsPageAssets/photo/photo-angelo.jpg";
 import annaFoto from "../../assets/images/devsPageAssets/photo/photo-anna.jpg";
+import marcoFoto from "../../assets/images/devsPageAssets/photo/photo-marco.jpg";
+import riccardoFoto from "../../assets/images/devsPageAssets/photo/photo-riccardo.jpg";
 import stefaniaFoto from "../../assets/images/devsPageAssets/photo/photo-stefania.jpg";
 
 export const developers = [
   {
     id: "1",
-    photo: "photo",
+    photo: angeloFoto,
     name: "Angelo Carnemolla",
     qrcode: angelo,
   },
@@ -22,13 +25,13 @@ export const developers = [
   },
   {
     id: "3",
-    photo: "photo",
+    photo: marcoFoto,
     name: "Marco Esu",
     qrcode: marco,
   },
   {
     id: "4",
-    photo: "photo",
+    photo: riccardoFoto,
     name: "Riccardo Priolo",
     qrcode: riccardo,
   },
