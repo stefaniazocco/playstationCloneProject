e1807a9c789129579b29858ab6f6ac19d393f5c0
unknown <annasantoriello15@gmail.com>
2022-12-19 12:00:12 +0100

    fix: translated cookies section

--- src/components/cookies/Cookies.js
+++ src/components/cookies/Cookies.js
@@ -1,30 +1,28 @@
 import { useEffect, useState } from "react";
 import cookies from "../../assets/images/homepage/logo-cookies.png";
 import xIcon from "../../assets/images/homepage/x-icon.png";
+import { useTranslation } from "react-i18next";
 
 export function Cookies({hide, accept}){
 
-    
+    const { t } = useTranslation();
    
 
     return(
         <>      
         <section id="cookies-section">
             <img className="logo-cookies" src={cookies} />
-            <div className="cookies-text">Sony Interactive Entertainment Europe Limited utilizza i cookie per personalizzare la
-                tua esperienza e gli annunci pubblicitari su questo sito Web e su siti Web di terze parti, analizzare i
-                dati, migliorare le prestazioni del sito e consentire la condivisione sui social media. Per ulteriori
-                informazioni o per modificare le impostazioni, <a href="#">consulta la nostra Informativa sui cookie</a> .
+            <div className="cookies-text">{t("first-cookies")} <a href="#">{t("link-cookies")}</a> .
             </div>
             <div className="cookies-buttons">
                 <div className="cookies-Button" onClick={hide}>
                     <a href="#">
-                        <div>Rifiuta</div>
+                        <div>{t("btn1")}</div>
                     </a>
                 </div>
                 <div className="cookies-Button" onClick={accept}>
                     <a href="#">
-                        <div>Accetta</div>
+                        <div>{t("btn2")}</div>
                     </a>
                 </div>
             </div>
--- src/i18n.js
+++ src/i18n.js
@@ -25,7 +25,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    fallbackLng: 'it',
+    fallbackLng: 'en',
     supportedLngs: ['it', 'en'],
     // lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
