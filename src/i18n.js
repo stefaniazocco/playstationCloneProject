a7f0c0975b6c72790af1c4110cf1cd31870730c2
stefaniazocco <stefaniazocco93@gmail.com>
2022-12-19 12:03:32 +0100

    fix: languages

--- src/i18n.js
+++ src/i18n.js
@@ -26,7 +26,7 @@ i18n
   .init({
     resources,
     fallbackLng: 'en',
-    supportedLngs: ['it', 'en'],
+    supportedLngs: ['en', 'it'],
     // lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
