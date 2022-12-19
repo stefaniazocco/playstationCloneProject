3921ad4e218b6cea4d23e260a675135a3c38e79e
unknown <annasantoriello15@gmail.com>
2022-12-19 14:53:56 +0100

    fix: fixed translated items

--- src/components/elements/cards/CardsStoreHomepage.js
+++ src/components/elements/cards/CardsStoreHomepage.js
@@ -1,12 +1,16 @@
 import { Button } from "../buttons/Button";
 import { ButtonSelf } from "../buttons/ButtonSelf";
+import i18n from "../../../i18n";
+import { useTranslation } from "react-i18next";
 
 export function CardsStoreHomepage({img, product, link,string}) {
+    const { t } = useTranslation();
+
     return(
         <div class="store-card">
             <img src={img} alt={product}/>
             <p>{product}</p>
-            <ButtonSelf className="button-red buttonStore" link={link} string="Acquista ora" />
+            <ButtonSelf className="button-red buttonStore" link={link} string={t("acquista")}/>
         </div>
     )
 }
